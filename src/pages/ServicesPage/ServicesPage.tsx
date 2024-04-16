fd31f72ac25527cd82102253cce98c2d795d21ab
Roman Koriachko <roman_koriachko@epam.com>
2024-04-16 22:48:22 +0300

    change description in header

--- src/pages/ServicesPage/ServicesPage.tsx
+++ src/pages/ServicesPage/ServicesPage.tsx
@@ -27,11 +27,6 @@ const ServicesPage = (props: Props) => {
 			text: 'Fogrosa provides various types of fertilizers to meet the needs of agriculture and other related...',
 			link: 'tolling',
 		},
-		// {
-		// 	title: 'Consulting',
-		// 	text: 'We provide consulting services to help our clients make informed decisions related to their businesses. Our...',
-		// 	link: 'consulting',
-		// },
 	];
 
 	const parallaxLeft = useParallax<HTMLDivElement>({
